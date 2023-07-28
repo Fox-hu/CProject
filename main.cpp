57223a409c1e2c66a5f350215b5a42c6f9686f09
fox <hujia0523@gmail.com>
2023-07-28 16:49:30 +0800

    add method adapter demo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 //
 
 #include <iostream>
+#include <algorithm>
 //#include "Student.h"
+#include "vector"
 #include "Student.cpp"
 
 using namespace std;
@@ -22,11 +24,26 @@ public:
         this->x = x;
         this->y = y;
     }
+
     //返回可以修改的引用左值 注意 这里和java不一样
     int &getX() {
         return this->x;
     }
 
+};
+
+//函数对象
+class Equal {
+private:
+    int threshold;
+public:
+    Equal(int threshold) {
+        this->threshold = threshold;
+    }
+
+    bool operator()(int number) {
+        return threshold == number;
+    }
 };
 
 int main() {
@@ -40,10 +57,10 @@ int main() {
     swap(number1, number2);
     cout << "number1 = " << number1 << " number2 = " << number2 << endl;
     //注意这个例子
-    Point point = Point(1,2);
+    Point point = Point(1, 2);
     //注意 这里 getx返回的是引用 直接可以修改 和java不一样
     point.getX() = 3;
-    cout << "point x = " << point.getX()  << endl;
+    cout << "point x = " << point.getX() << endl;
 
     //malloc free new delete 都是开辟和释放内存 malloc/free配对 new和delete配对
     //malloc free不会去调用构造函数和析构函数
@@ -72,6 +89,18 @@ int main() {
     //第一个参数 start 第二个参数 end 默认是字符串结尾
     str1.erase(3);
     cout << str1.c_str() << endl;
+
+    //预定义函数对象和函数适配器
+    vector<int> vector1;
+    vector1.push_back(1);
+    vector1.push_back(2);
+    vector1.push_back(3);
+    vector1.push_back(2);
+    vector1.push_back(4);
+    vector1.push_back(2);
+
+    int count = count_if(vector1.begin(), vector1.end(),Equal(2));
+    cout << "count = " << count << endl;
 
     return 0;
 }
